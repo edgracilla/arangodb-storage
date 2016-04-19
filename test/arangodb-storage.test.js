8669772e4242791209d210bd01d1b218c5099ac8
Benj Sicam <bsicam@reekoh.com>
2016-04-19 12:04:08 +0800

    v1.0.0 Updates

--- test/arangodb-storage.test.js
+++ test/arangodb-storage.test.js
@@ -1,25 +1,21 @@
-/*
- * Just a sample code to test the storage plugin.
- * Kindly write your own unit tests for your own plugin.
- */
 'use strict';
 
-var cp       = require('child_process'),
-	assert   = require('assert'),
-	should   = require('should'),
-	moment   = require('moment'),
+var cp     = require('child_process'),
+	assert = require('assert'),
+	should = require('should'),
+	moment = require('moment'),
 	storage;
 
 
-var HOST 	        = '52.91.168.197',
-	USER 	        = 'rozzwalla',
+var HOST            = '52.91.168.197',
+	USER            = 'rozzwalla',
 	PASSWORD        = 'reekoh',
-	PORT 	        = 8529,
+	PORT            = 8529,
 	CONNECTION_TYPE = 'http',
 	DATABASE        = '_system',
 	COLLECTION      = 'reekoh_collection',
 	COLLECTION_TYPE = 'collection',
-	ID  	        = new Date().getTime().toString();
+	ID              = new Date().getTime().toString();
 
 var record = {
 	_key: ID,
@@ -31,7 +27,6 @@ var record = {
 	random_data: 'abcdefg',
 	is_normal: true
 };
-
 
 describe('Storage', function () {
 	this.slow(5000);
@@ -68,14 +63,14 @@ describe('Storage', function () {
 				type: 'ready',
 				data: {
 					options: {
-						host	          : HOST,
-						port              : PORT,
-						user	          : USER,
-						password          : PASSWORD,
-						connection_type   : CONNECTION_TYPE,
-						database          : DATABASE,
-						collection        : COLLECTION,
-						collection_type   : COLLECTION_TYPE
+						host: HOST,
+						port: PORT,
+						user: USER,
+						password: PASSWORD,
+						connection_type: CONNECTION_TYPE,
+						database: DATABASE,
+						collection: COLLECTION,
+						collection_type: COLLECTION_TYPE
 					}
 				}
 			}, function (error) {
@@ -98,23 +93,22 @@ describe('Storage', function () {
 			this.timeout(20000);
 
 			var Database = require('arangojs').Database,
-				db = new Database(CONNECTION_TYPE + '://' + USER + ':' + PASSWORD + '@' + HOST + ':' + PORT),
+				db       = new Database(CONNECTION_TYPE + '://' + USER + ':' + PASSWORD + '@' + HOST + ':' + PORT),
 				collection;
 			db.useDatabase(DATABASE);
 
-            if (COLLECTION_TYPE === 'collection')
-		    	collection = db.collection(COLLECTION);
+			if (COLLECTION_TYPE === 'collection')
+				collection = db.collection(COLLECTION);
 			else
 				collection = db.edgeCollection(COLLECTION);
 
-			collection.document(ID, function (err, result)
-				{
+			collection.document(ID, function (err, result) {
 					should.equal(record.co2, result.co2, 'Data validation failed. Field: co2');
 					should.equal(record.temp, result.temp, 'Data validation failed. Field: temp');
 					should.equal(record.quality, result.quality, 'Data validation failed. Field: quality');
 					should.equal(record.random_data, result.random_data, 'Data validation failed. Field: random_data');
 					should.equal(moment(record.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'),
-						moment(result.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'),  'Data validation failed. Field: reading_time');
+						moment(result.reading_time).format('YYYY-MM-DDTHH:mm:ss.SSSSZ'), 'Data validation failed. Field: reading_time');
 					should.equal(JSON.stringify(record.metadata), JSON.stringify(result.metadata), 'Data validation failed. Field: metadata');
 					should.equal(record.is_normal, result.is_normal, 'Data validation failed. Field: is_normal');
 
